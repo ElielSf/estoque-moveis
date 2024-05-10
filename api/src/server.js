ec97661e7d12988b76aad969a0af57bfb9b460dd
Eliel Souza <elielsf13@gmail.com>
2024-05-10 18:08:52 -0300

    comecar a criar o backend

---
+++ api/src/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import registerRoute from './routes/Register.js'
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/cadastrar', registerRoute);
+
+
+
+try {
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando em https://localhost:${PORT}`)
+    });
+} catch (err) {
+    console.log('erro ao rodar o servidor: ' + err);
+}
